39fa3b3a4c044f53764e00ae93a1b48c016af211
ThawZinToe <developer.ptut@gmail.com>
2024-04-11 19:38:27 +0700

    feat: update design file into ui module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:ui"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ core/ui/src/main/java/com/ptut/insightify/ui/InsightifyIcons.kt
@@ -0,0 +1,32 @@
+package com.ptut.insightify.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Bookmarks
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Bookmark
+import androidx.compose.material.icons.rounded.BookmarkBorder
+import androidx.compose.material.icons.rounded.Bookmarks
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Grid3x3
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.icons.rounded.Settings
+
+object InsightifyIcons {
+    val Add = Icons.Rounded.Add
+    val ArrowBack = Icons.Rounded.ArrowBack
+    val Bookmark = Icons.Rounded.Bookmark
+    val BookmarkBorder = Icons.Rounded.BookmarkBorder
+    val Bookmarks = Icons.Rounded.Bookmarks
+    val BookmarksBorder = Icons.Outlined.Bookmarks
+    val Check = Icons.Rounded.Check
+    val Close = Icons.Rounded.Close
+    val Grid3x3 = Icons.Rounded.Grid3x3
+    val MoreVert = Icons.Default.MoreVert
+    val Person = Icons.Rounded.Person
+    val Search = Icons.Rounded.Search
+    val Settings = Icons.Rounded.Settings
+}
